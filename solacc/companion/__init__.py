932e43dfba33b7e600c7d7e7c350b0d3bea9ddd2
dbbnicole <86326159+dbbnicole@users.noreply.github.com>
2022-08-12 19:23:53 -0500

    Update __init__.py

--- solacc/companion/__init__.py
+++ solacc/companion/__init__.py
@@ -40,11 +40,13 @@ class NotebookSolutionCompanion():
                      "new_settings": params}
       json_response = client.execute_post_json(f"{client.endpoint}/api/2.1/jobs/reset", reset_params) # returns {} if status is 200
       assert json_response == {}, "Job reset returned non-200 status"
-      displayHTML(f"""Reset the <a href="/#job/{job_id}" target="_blank">{params["name"]}</a> job to original definition""")
+      print(f"""Reset the <a href="/#job/{job_id}" target="_blank">{params["name"]}</a> job to original definition""")
+#       displayHTML(f"""Reset the <a href="/#job/{job_id}" target="_blank">{params["name"]}</a> job to original definition""")
     else:
       json_response = client.execute_post_json(f"{client.endpoint}/api/2.1/jobs/create", params)
       job_id = json_response["job_id"]
-      displayHTML(f"""Created <a href="/#job/{job_id}" target="_blank">{params["name"]}</a> job""")
+      print(f"""Created <a href="/#job/{job_id}" target="_blank">{params["name"]}</a> job""")
+#       displayHTML(f"""Created <a href="/#job/{job_id}" target="_blank">{params["name"]}</a> job""")
     return job_id
   
   # Note these functions assume that names for solacc jobs/cluster/pipelines are unique, which is guaranteed if solacc jobs/cluster/pipelines are created from this class only
@@ -93,12 +95,14 @@ class NotebookSolutionCompanion():
       if cluster_id: 
         params["cluster_id"] = cluster_id
         edit_cluster(client, cluster_id, params)
-        displayHTML(f"""Reset the <a href="/#setting/clusters/{cluster_id}/configuration" target="_blank">{params["cluster_name"]}</a> job to original definition""")
+        print(f"""Reset the <a href="/#setting/clusters/{cluster_id}/configuration" target="_blank">{params["cluster_name"]}</a> job to original definition""")
+#         displayHTML(f"""Reset the <a href="/#setting/clusters/{cluster_id}/configuration" target="_blank">{params["cluster_name"]}</a> job to original definition""")
         
       else:
         json_response = client.execute_post_json(f"{client.endpoint}/api/2.0/clusters/create", params)
         cluster_id = json_response["cluster_id"]
-        displayHTML(f"""Created <a href="/#setting/clusters/{cluster_id}/configuration" target="_blank">{params["cluster_name"]}</a> cluster""")
+        print(f"""Created <a href="/#setting/clusters/{cluster_id}/configuration" target="_blank">{params["cluster_name"]}</a> cluster""")
+#         displayHTML(f"""Created <a href="/#setting/clusters/{cluster_id}/configuration" target="_blank">{params["cluster_name"]}</a> cluster""")
       return 
     
   @staticmethod
