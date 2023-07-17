2647309373e2d485e866fd735a669f19efa70ccd
dbbnicole <86326159+dbbnicole@users.noreply.github.com>
2023-07-17 11:00:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='solacc',
     version='0.1',
     packages=find_packages(exclude=["tests", "tests.*"]),
-    install_requires=["databricks-sdk>=0.1.12", "urllib3>=1.26.9"],
+    install_requires=["databricks-sdk>=0.1.12", "urllib3==1.26.9"],
     license_files = ('LICENSE',)
 )
